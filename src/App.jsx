16b70a052ee8c165b666746acb7d95fbea16f589
Ilya Nesterovich <manager@inesterovich.ru>
2021-03-22 20:59:14 +0300

    feat:update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,28 @@
-import MainPage from './pages/MainPage'
-import TextBook from './pages/TextBookPage'
+
 import { ThemeProvider, Container } from "@material-ui/core";
 import { theme } from "./theme";
 import { Header } from './views/Header/Header'
-import {Switch, Route, Redirect} from "react-router-dom";
-import RegisterPage from "./pages/Register";
-import {LoginPage} from "./pages/Login/LoginPage";
-import {useDispatch, useSelector} from "react-redux";
-import {logout} from "./redux/auth/thunks";
-import MyWordsPage from "./pages/MyWordsPage";
+import { useSelector } from "react-redux";
+import { useRoutes } from './routes';
 
 
 function App() {
-    const name = useSelector( state => state.auth.userInfo?.name)
-    const dispatch = useDispatch()
-    const handleClick = () => {
-        dispatch(logout())
-    }
+   
+    const isAuthenticated = !!useSelector(state => state.auth.userInfo?.token);
+   
+
+    const routes = useRoutes(isAuthenticated);
 
     return (
       <ThemeProvider theme={theme}>
         <div className="App">
           <Header />
           <Container maxWidth="lg">
-            {name
-                ? <>{name} <button onClick={handleClick}>Log out</button></>
-                : <Redirect to={"/login"}/>
-            }
+                    <main>
+                        {routes}
+                    </main>
             
-            <Switch>
-                <Route exact path="/">
-                    <MainPage/>
-                </Route>
-                <Route path="/textbook">
-                    <TextBook/>
-                </Route>
-                <Route path={"/register"}>
-                    <RegisterPage/>
-                </Route>
-                <Route path={"/login"}>
-                    <LoginPage/>
-                </Route>
-                <Route path={"/my-words"}>
-                    <MyWordsPage/>
-                </Route>
-            </Switch>
+ 
             </Container>
         </div>
       </ThemeProvider>
