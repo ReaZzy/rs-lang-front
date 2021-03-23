0b80f1dd42645a48128fd269fbe794d1988186aa
Ilya Nesterovich <manager@inesterovich.ru>
2021-03-23 15:52:05 +0300

    feat: add TexBookValidation

--- src/pages/TextBookPage/TextBookPage.jsx
+++ src/pages/TextBookPage/TextBookPage.jsx
@@ -1,15 +1,65 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { Pagination } from '@material-ui/lab';
+import PaginationItem from '@material-ui/lab/PaginationItem';
+
 
 export const TextBookPage = () => {
 
+ 
   let { module, page } = useParams();
+
+  const [isUrlReady, setUrlBool] = useState(false);
+  const history = useHistory();
+  const location = useLocation();
+
+  const validationHandler = useCallback(
+
+    () => {
+      let redirectModule = Number(module);
+      let redirectPage = Number(page);
   
+      if (redirectModule === 0 || redirectModule > 6) {
+        redirectModule = 1;
+      }
+  
+      if (redirectPage === 0 || redirectPage > 30 || !redirectPage) {
+        redirectPage = 1;
+      }
+  
+  
+      return `/textbook/${redirectModule}/${redirectPage}`
+  
+    }, [module, page])
+    
+  useEffect(() => {
+    const redirectUrl = validationHandler();
+    if (redirectUrl !== location.pathname) {
+      history.replace(redirectUrl);
+    }
+    
+    setUrlBool(true);
+    
+    }, [history, location.pathname, validationHandler])
+
+ 
+  if (!isUrlReady) {
+    return (
+      <div>
+        <p>Page is Loading</p>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h1>Раздел {module}</h1>
-      <h2> Страница {page }</h2>
+      <p>Page content</p>
+      <Pagination count={30}
+      />
+
+    
     </div>
   )
+
+  
 }
